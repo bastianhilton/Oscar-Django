166e5c9392421a177b0d2546709184bfa9cdbb81
Hilton <sehilton@amazon.com>
2021-09-01 01:08:10 -0700

    fixed procfile causing heroku fail

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn core.wsgi --log-file=- 
+web: gunicorn --chdir FromThePath FromThePath.wsgi
--- shop/wsgi.py
+++ shop/wsgi.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-import subprocess
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop.settings')
